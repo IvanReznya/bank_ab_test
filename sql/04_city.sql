6429e06e80fbc4b6c7f99540e39b1656e67a2bdf
IvanReznya <renzyaevi@yandex.ru>
2026-08-17 20:49:03 +0300

    Update 04_city.sql

--- sql/04_city.sql
+++ sql/04_city.sql
@@ -5,6 +5,6 @@ count(u.user_id) as users,
 round(count(u.user_id) * 100.0 / sum(count(*)) over (partition by e.experiment_group), 2) as users_percent
 from experiment e 
 left join users u on e.user_id  = u.user_id 
-where u.city is not null and u.city != ''
+where nullif(trim(u.city), '') is not null
 group by e.experiment_group, u.city
 order by e.experiment_group, u.city
